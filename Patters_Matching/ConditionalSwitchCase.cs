4710730163c082273a9c34988e129b9311fa16c1
jvargasipince <jvargas.ipince@gmail.com>
2017-08-12 16:19:55 -0500

    Agregar archivos de proyecto.

---
+++ Patters_Matching/ConditionalSwitchCase.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Patters_Matching
+{
+    class ConditionalSwitchCase
+    {
+        static void Main(string[] args)
+        {
+
+            Planilla planilla = new Planilla();
+            planilla.execute();
+
+            Write(Environment.NewLine);
+            planilla.SwitchDemo(DateTime.Today);
+
+            Write(Environment.NewLine);
+            planilla.SwitchDemo("AS");
+
+            Write(Environment.NewLine);
+            planilla.SwitchDemo(10);
+        }
+    }
+
+    public class Planilla
+    {
+        public void execute()
+        {
+            Empleado empleado = new ArquitectoSw();
+            empleado.Nombre = "Jorge Vargas";
+            empleado.AñosExperiencia = 8;
+            (empleado as ArquitectoSw).Proyecto = "Migración a Azure";
+            (empleado as ArquitectoSw).PersonasACargo = 5;
+            (empleado as Developer).Lenguage = "C#, Angular, TypeScript";
+            (empleado as Developer).Nivel = "Senior";
+
+            switch (empleado)
+            {
+                case ArquitectoSw arq:
+                    Write($" El empleado {arq.Nombre} ");
+                    Write(Environment.NewLine);
+                    Write($" Con {arq.AñosExperiencia} años de experiencia ");
+                    Write(Environment.NewLine);
+                    Write($" Pertenece al proyecto {arq.Proyecto} ");
+                    Write(Environment.NewLine);
+                    Write($" Con {arq.PersonasACargo} personas a cargo ");
+                    Write(Environment.NewLine);
+                    Write($" Maneja los lenguajes {arq.Lenguage} ");
+                    Write(Environment.NewLine);
+                    Write($" A un nivel de {arq.Nivel}");
+                    break;
+
+                case Developer dev:
+                    Write($" El empleado {dev.Nombre}");
+                    Write(Environment.NewLine);
+                    Write($"Con {dev.AñosExperiencia} años de experiencia");
+                    Write(Environment.NewLine);
+                    Write($"Maneja los lenguajes {dev.Lenguage}");
+                    Write(Environment.NewLine);
+                    Write($"A un nivel de {dev.Nivel}");
+                    break;
+
+                case Empleado emp:
+                    Write($" El empleado {emp.Nombre}");
+                    Write(Environment.NewLine);
+                    Write($" Con {emp.AñosExperiencia} años de experiencia");
+                    Write(Environment.NewLine);
+                    Write($" No es developer ni arquitecto");
+                    break;
+            }
+
+        }
+
+        public void SwitchDemo(object c)
+        {
+
+            switch (c)
+            {
+                case string texto:
+                    Write($" El valor {texto} es una cadena");
+                    break;
+
+                case int entero:
+                    Write($" El valor {entero} es una entero");
+                    break;
+
+                case DateTime date:
+                    Write($" El valor {date} es una fecha");
+                    break;
+            }
+
+
+        }
+        //Compilar con control + F5       
+    }
+
+    public class Empleado
+    {
+        public string Nombre { get; set; }
+        public int AñosExperiencia { get; set; }
+    }
+
+    public class Developer : Empleado
+    {
+        public string Nivel { get; set; }
+        public string Lenguage { get; set; }
+    }
+
+    public  class ArquitectoSw : Developer
+    {
+        public string Proyecto;
+        public int PersonasACargo;
+
+    }
+}
---
+++ Patters_Matching/TypePatterns.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Patters_Matching
+{
+    class TypePatterns
+    {
+        static void Main(string[] args)
+        {
+
+            Factorial factorial = new Factorial();
+            factorial.execute();
+
+        }
+
+
+        public class Factorial
+        {
+            public void execute()
+            {
+                CalcularFactorial(4);
+                CalcularFactorialIncluyendoCadena("5");
+
+                //Compilar con control + F5
+            }
+
+            public void CalcularFactorial(object o)
+            {
+                
+                //int.TryParse() => no es necesario validarlo de esta forma
+                if (o is null) return; // constant pattern
+                if (!(o is int numero)) return; //type pattern (int)
+
+                int total = 1;
+                for (int contador = 1; contador <= numero; contador++)
+                {
+                    total *= contador;
+                }
+
+                WriteLine($"El factorial de {numero} es {total}");
+                //ReadLine();
+            }
+
+            public void CalcularFactorialIncluyendoCadena(object o)
+            {
+
+                //int.TryParse() => no es necesario validarlo de esta forma
+                if (o is null) return;
+                if (!(o is int numero || o is string cadena && int.TryParse(cadena, out numero))) return;
+
+                int total = 1;
+                for (int contador = 1; contador <= numero; contador++)
+                {
+                    total *= contador;
+                }
+
+                WriteLine($"El factorial de {numero} es {total}");
+                //ReadLine();
+            }
+
+        }
+
+    }
+}
